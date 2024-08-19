b7754cdcce31c94fc4e9b53763dcd88d2504aa2e
Paulo <106405308+Paulo19961944@users.noreply.github.com>
2024-08-19 15:53:10 -0300

    feature: Add New Button and Display LCD 16x2 I2C

--- KeyPadAccess.ino
+++ KeyPadAccess.ino
@@ -1,10 +1,14 @@
-// INCLUSÃO DA BIBLIOTECA KEYPAD
+// INCLUSÃO DE BIBLIOTECAS
 #include <Keypad.h>
+#include <LiquidCrystal_I2C.h>
 
 // VARIAVEIS GLOBAIS
 const int MAX_ATTEMPTS = 3; // Número máximo de tentativas
 const char *CORRECT_CODE = "122024"; // Código correto
-const byte PIN_RELAY = 3;
+const byte PIN_RELAY = 3; // Pino do Relé
+const byte PIN_BUTTON = 2; // Pino do Botão
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Endereço do I2C e dimensões do LCD (colunas, linhas)
 
 // TECLAS DO KEYPAD
 char keyMap[][4] = {
@@ -17,8 +21,9 @@ char keyMap[][4] = {
 // CONFIGURAÇÃO DO KEYPAD
 byte rows[] = {12, 11, 10, 9};
 byte cols[] = {7, 6, 5, 4};
-int tam = sizeof(rows) / sizeof(rows[0]);
-Keypad kdp(makeKeymap(keyMap), rows, cols, tam, tam);
+int rowCount = sizeof(rows) / sizeof(rows[0]);
+int colCount = sizeof(cols) / sizeof(cols[0]);
+Keypad kdp(makeKeymap(keyMap), rows, cols, rowCount, colCount);
 
 // TENTATIVAS
 String inputCode = ""; // Armazena o código digitado
@@ -29,13 +34,21 @@ bool accessBlocked = false; // Flag para indicar se o acesso está bloqueado
 void setup() {
   Serial.begin(9600); // Inicializa a Serial
   pinMode(PIN_RELAY, OUTPUT);
+  pinMode(PIN_BUTTON, INPUT_PULLUP);
+  lcd.begin(16, 2); // Inicializa o LCD com 16 colunas e 2 linhas
+  lcd.backlight(); // Liga a luz de fundo do LCD
+  digitalWrite(PIN_RELAY, LOW); // Garante que o relé comece apagado
 }
 
 // FUNÇÃO LOOP
 void loop() {
-  // BLOQUEIA O ACESSO
+  if (digitalRead(PIN_BUTTON) == LOW) { // Botão pressionado
+    digitalWrite(PIN_RELAY, LOW); // Desliga o relé
+    delay(1000); // Pequeno atraso para evitar leitura constante
+  }
+
   if (accessBlocked) {
-    // Se o acesso está bloqueado, não faz nada
+    // Se o acesso está bloqueado, não faz nada além de verificar o botão
     return;
   }
 
@@ -43,25 +56,30 @@ void loop() {
 }
 
 // FUNÇÃO DE ACESSO DO KEYPAD
-void keyPadAccess(){
+void keyPadAccess() {
   char key = kdp.getKey(); // Captura a tecla do Keypad
 
   // SE CLICAR EM ALGUMA TECLA
   if (key != NO_KEY) {
+    lcd.clear(); // Limpa o LCD para mostrar a nova mensagem
+
     if (key == '#') { // # É usado para confirmar o código
       if (inputCode == CORRECT_CODE) {
-        Serial.println("Acesso Liberado");
         inputCode = ""; // Reseta o código após sucesso
         attempts = 0; // Reseta as tentativas
-        digitalWrite(PIN_RELAY, HIGH);
+        digitalWrite(PIN_RELAY, HIGH); // Liga o relé
+        lcd.print("Acesso Liberado");
+        clearLCD();
       } else {
         attempts++;
         if (attempts >= MAX_ATTEMPTS) {
-          Serial.println("Acesso Bloqueado");
+          lcd.print("Acesso Bloqueado");
           accessBlocked = true; // Define a flag para bloquear o acesso
         } else {
-          Serial.println("Acesso Negado");
+          lcd.print("Acesso Negado");
+          clearLCD();
         }
+
         inputCode = ""; // Reseta o código após falha
       }
     } else if (key == '*') { // * É usado para apagar o último caractere
@@ -72,4 +90,10 @@ void keyPadAccess(){
       inputCode += key; // Adiciona a tecla digitada ao código
     }
   }
+}
+
+// LIMPA O DISPLAY
+void clearLCD(){
+  delay(1000); // Espera 1 segundo
+  lcd.clear(); // Limpa o Display
 }
